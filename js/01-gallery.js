12ee5022e76e0cbfd31f43414877741b4625d2f6
Oleh Semennykov <sumcue@gmail.com>
2021-10-23 19:59:13 +0300

    first task is over

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,74 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryItemMarkup(); 
+galleryContainer.insertAdjacentHTML('beforeend',cardsMarkup);
+
+// console.log(createGalleryItemMarkup());
+// рендер и создание елементов
+function createGalleryItemMarkup () {
+   
+    return galleryItems.map(({preview, original, description}) => {
+ return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}).join(" ");
+}
+//  делегирование
+const refs = {
+     link : document.querySelector(".gallery__link"),
+   item : document.querySelector(".gallery__item"),
+  image : document.querySelector(".gallery__image"),
+}
+
+// console.log(refs.link);
+// console.log(refs.item);
+// console.log(galleryImage);
+// const  handleImageClick = () => {
+// console.log('this is click')
+// };
+
+
+const instance = basicLightbox.create(
+    `<img src=""/>`
+    
+, {
+    onShow: () => {
+        window.addEventListener('click', handleKeyDown);
+},
+onClose: () =>{
+    window.addEventListener('click', handleKeyDown)
+},
+
+}
+)
+
+
+
+const handleImageClick =(event) => {
+event.preventDefault();
+instance.element().querySelector("img").src = event.target.dataset.source;
+instance.show()
+console.log()
+};
+ const handleKeyDown = (event) => {
+     if(event.key === 'Escape')
+     instance.close();
+ }
+
+galleryContainer.addEventListener('click', handleImageClick);
+ 
+
+
+
+
+
+
