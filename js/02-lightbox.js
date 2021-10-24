1f7562835b5b892d4d6374dfbf4bf3da85b28e04
Oleh Semennykov <sumcue@gmail.com>
2021-10-24 15:54:50 +0300

    add librery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
+console.log(galleryContainer);
 console.log(galleryItems);
+
+const cardsMarkup = createGalleryItemMarkup(); 
+galleryContainer.insertAdjacentHTML('beforeend',cardsMarkup);
+
+
+function createGalleryItemMarkup () {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description} " />
+      </a>`
+       ;
+
+    }).join(' ');
+}
+
+let lightbox = new SimpleLightbox(".gallery a");
+lightbox.on('show.simplelightbox', 
+function handleModalOpen(event)  {
+event.preventDefault();
+	
+})
+console.log(lightbox);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
